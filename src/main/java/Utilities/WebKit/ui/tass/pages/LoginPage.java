a38bf625ac2267c9b489a1dbb48e1487e0b33303
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:13:46 +0530

    Update LoginPage.java

--- src/main/java/Utilities/WebKit/ui/tass/pages/LoginPage.java
+++ src/main/java/Utilities/WebKit/ui/tass/pages/LoginPage.java
@@ -1,4 +1,31 @@
-package Utilities.WebKit.ui.tass.pages;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.ui.taas.pages;
+
+import com.test.seleniumcustomframework.extension.PageElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
+    @FindBy(
+        className = "list-title"
+    )
+    public PageElement allUpdates;
+    @FindBy(
+        className = "aui-header-logo-device"
+    )
+    public PageElement confluenceLogo;
+
+    public LoginPage() {
+    }
+
+    public void verifyLabel() {
+        this.allUpdates.isDisplayed();
+    }
+
+    public void clickConfluence() {
+        this.confluenceLogo.click();
+    }
 }
