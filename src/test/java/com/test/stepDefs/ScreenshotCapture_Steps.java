2510d005d8bca4839bbac35dde1ee9e49aaae873
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:29:44 +0530

    Create ScreenshotCapture_Steps.java

---
+++ src/test/java/com/test/stepDefs/ScreenshotCapture_Steps.java
@@ -0,0 +1,27 @@
+package com.test.stepDefs;
+
+import com.google.inject.Inject;
+import com.test.helper.BasePage;
+import com.test.pages.SceenshotCapturePage;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class ScreenshotCapture_Steps extends BasePage {
+
+    @Inject
+    SceenshotCapturePage sceenshotCapturePage;
+
+    @When("^Login to Jenkins Customized Job")
+    public void hitJenkinsUrl(){
+        sceenshotCapturePage.hitCustomisedURL();
+        sceenshotCapturePage.isValidPageDisplayed();
+        sceenshotCapturePage.submitValidCredentials();
+        Assert.assertTrue(sceenshotCapturePage.isValidPageDisplayedDashboards());
+    }
+
+    @Given("^capture report Screenshots")
+    public void capture_Report_Screenshot() throws Throwable {
+        sceenshotCapturePage.takeReportScreenshots();
+    }
+}
