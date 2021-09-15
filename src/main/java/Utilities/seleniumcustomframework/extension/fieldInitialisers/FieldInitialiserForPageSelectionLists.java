bb8cbe6f7c7513c92fe4637019dd95c8c957973b
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:27:47 +0530

    Update FieldInitialiserForPageSelectionLists.java

--- src/main/java/Utilities/seleniumcustomframework/extension/fieldInitialisers/FieldInitialiserForPageSelectionLists.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/fieldInitialisers/FieldInitialiserForPageSelectionLists.java
@@ -1,51 +1,56 @@
-package Utilities.seleniumcustomframework.extension.fieldInitialisers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.seleniumcustomframework.extension.PageFactory;
-import Utilities.seleniumcustomframework.extension.annotations.Section;
-import Utilities.seleniumcustomframework.extension.dependencies.DependencyInjector;
-import Utilities.seleniumcustomframework.extension.exceptions.PageFactortError;
-import Utilities.seleniumcustomframework.extension.handlers.PageSectionListHandler;
-import Utilities.seleniumcustomframework.extension.helpers.FrameWeapper;
-import Utilities.seleniumcustomframework.extension.orchestration.WebDriverFrameSwitchingOrchestrator;
+package com.test.seleniumcustomframework.extension.fieldInitialisers;
+
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import org.openqa.selenium.SearchContext;
-import org.openqa.selenium.support.pagefactory.Annotations;
-
+import com.test.seleniumcustomframework.extension.PageFactory;
+import com.test.seleniumcustomframework.extension.annotations.Section;
+import com.test.seleniumcustomframework.extension.dependencies.DependencyInjector;
+import com.test.seleniumcustomframework.extension.exceptions.PageFactoryError;
+import com.test.seleniumcustomframework.extension.handlers.PageSectionListHandler;
+import com.test.seleniumcustomframework.extension.helpers.FrameWrapper;
+import com.test.seleniumcustomframework.extension.orchestration.WebDriverFrameSwitchingOrchestrator;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
 import java.util.List;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.support.pagefactory.Annotations;
 
-public class FieldInitialiserForPageSelectionLists implements FieldInitialiser {
+public class FieldInitialiserForPageSectionLists implements FieldInitialiser {
     @Inject
-    private DependencyInjector dependencyInjector=null;
+    private DependencyInjector dependencyInjector = null;
     @Inject
-    private Provider<PageFactory> pageFactory= null;
+    private Provider<PageFactory> pageFactory = null;
     @Inject
-    private WebDriverFrameSwitchingOrchestrator webDriverFrameSwitchingOrchestrator =null;
+    private WebDriverFrameSwitchingOrchestrator webDriverFrameSwitchingOrchestrator = null;
 
-    public FieldInitialiserForPageSelectionLists(){
+    public FieldInitialiserForPageSectionLists() {
     }
 
-    public Boolean initialiseField(Field field, Object page, SearchContext searchContext, FrameWeapper frame) throws PageFactortError {
-      if (!FieldAssessor.isValidPageSelectionList(field)){
-        return false;
-        }else {
-          ParameterizedType genericTypeImpl= (ParameterizedType) field.getGenericType();
-          Type genericTypeArgument =genericTypeImpl.getActualTypeArguments()[0];
-          Annotations annotations =new Annotations(field);
-          PageSectionListHandler pageSelectionListHandle=new PageSectionListHandler(this.dependencyInjector,searchContext,annotations.buildBy(),genericTypeArgument,this.pageFactory, frame, this.webDriverFrameSwitchingOrchestrator);
-          Object proxyInstance= Proxy.newProxyInstance(Section.class.getClassLoader(),new Class[]{List.class}, pageSelectionListHandle);
-          field.setAccessible(true);
+    public Boolean initialiseField(Field field, Object page, SearchContext searchContext, FrameWrapper frame) {
+        if (!FieldAssessor.isValidPageSectionList(field)) {
+            return false;
+        } else {
+            ParameterizedType genericTypeImpl = (ParameterizedType)field.getGenericType();
+            Type genericTypeArgument = genericTypeImpl.getActualTypeArguments()[0];
+            Annotations annotations = new Annotations(field);
+            PageSectionListHandler pageSectionListHandler = new PageSectionListHandler(this.dependencyInjector, searchContext, annotations.buildBy(), genericTypeArgument, this.pageFactory, frame, this.webDriverFrameSwitchingOrchestrator);
+            Object proxyInstance = Proxy.newProxyInstance(Section.class.getClassLoader(), new Class[]{List.class}, pageSectionListHandler);
+            field.setAccessible(true);
 
-          try {
-              field.set(page, proxyInstance);
-          }catch (IllegalAccessException var11){
-               throw new PageFactortError(var11.getCause());
-          }
-          return true;
-          }
-      }
+            try {
+                field.set(page, proxyInstance);
+            } catch (IllegalAccessException var11) {
+                throw new PageFactoryError(var11.getCause());
+            }
+
+            return true;
+        }
+    }
 }
