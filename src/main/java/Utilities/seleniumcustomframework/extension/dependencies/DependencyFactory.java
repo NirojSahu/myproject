6ad06cd9b8c29bf1f9f0370e75048c721e46a65f
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:22:55 +0530

    Update DependencyFactory.java

--- src/main/java/Utilities/seleniumcustomframework/extension/dependencies/DependencyFactory.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/dependencies/DependencyFactory.java
@@ -1,7 +1,12 @@
-package Utilities.seleniumcustomframework.extension.dependencies;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.seleniumcustomframework.extension.dependencies;
 
 import com.google.inject.Provider;
 
-public interface DependencyFactory<T> extends Provider {
+public interface DependencyFactory<T> extends Provider<T> {
     T get();
 }
