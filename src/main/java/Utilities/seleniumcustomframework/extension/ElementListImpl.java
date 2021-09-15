6df3cd09dc8e601a6a36b9075e960fcfeeeda116
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:29:17 +0530

    Update ElementListImpl.java

--- src/main/java/Utilities/seleniumcustomframework/extension/ElementListImpl.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/ElementListImpl.java
@@ -1,14 +1,19 @@
-package Utilities.seleniumcustomframework.extension;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import org.openqa.selenium.SearchContext;
+package com.test.seleniumcustomframework.extension;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import org.openqa.selenium.SearchContext;
 
 public class ElementListImpl {
-    public ElementListImpl(SearchContext searchContext, List WebElementListProxy){
+    public ElementListImpl(SearchContext searchContext, List webElementListProxy) {
     }
-    public boolean canHandle(Method method){
+
+    public boolean canHandle(Method method) {
         return false;
     }
 }
