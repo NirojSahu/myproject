21e7775545d7f8b2d1f466df534db54778f4e210
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:52:52 +0530

    Update Data.java

--- src/main/java/Responses/OBReadBalance1/Data.java
+++ src/main/java/Responses/OBReadBalance1/Data.java
@@ -1,37 +1,21 @@
-
 package Responses.OBReadBalance1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import javax.annotation.Generated;
-
-
 
 public class Data {
-
     @JsonProperty("Balance")
-    private List<Balance> mBalance;
+    private List<Balance> balance;
 
-    public List<Balance> getBalance() {
-        return mBalance;
+    public List<Balance> getBalance ()
+    {
+        return balance;
     }
 
-    public static class Builder {
-
-        private List<Balance> mBalance;
-
-        public Data.Builder withBalance(List<Balance> balance) {
-            mBalance = balance;
-            return this;
-        }
-
-        public Data build() {
-            Data data = new Data();
-            data.mBalance = mBalance;
-            return data;
-        }
-
-    }
-
+    /*@Override
+    public String toString()
+    {
+        return "ClassPojo [Balance = "+Balance+"]";
+    }*/
 }
