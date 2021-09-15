d444066c340f75c53db4789134d07dce05caf9e3
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:27:28 +0530

    Update FieldInitialiserForPageElements.java

--- src/main/java/Utilities/seleniumcustomframework/extension/fieldInitialisers/FieldInitialiserForPageElements.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/fieldInitialisers/FieldInitialiserForPageElements.java
@@ -1,47 +1,55 @@
-package Utilities.seleniumcustomframework.extension.fieldInitialisers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.seleniumcustomframework.extension.PageElement;
-import Utilities.seleniumcustomframework.extension.PageElementImpl;
-import Utilities.seleniumcustomframework.extension.dependencies.DependencyInjector;
-import Utilities.seleniumcustomframework.extension.handlers.WebElementHandler;
-import Utilities.seleniumcustomframework.extension.helpers.FrameWeapper;
-import Utilities.seleniumcustomframework.extension.orchestration.WebDriverFrameSwitchingOrchestrator;
+package com.test.seleniumcustomframework.extension.fieldInitialisers;
+
 import com.google.inject.Inject;
+import com.test.seleniumcustomframework.extension.PageElement;
+import com.test.seleniumcustomframework.extension.PageElementImpl;
+import com.test.seleniumcustomframework.extension.dependencies.DependencyInjector;
+import com.test.seleniumcustomframework.extension.handlers.WebElementHandler;
+import com.test.seleniumcustomframework.extension.helpers.FrameWrapper;
+import com.test.seleniumcustomframework.extension.orchestration.WebDriverFrameSwitchingOrchestrator;
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WrapsElement;
+import org.openqa.selenium.internal.WrapsElement;
 import org.openqa.selenium.support.pagefactory.Annotations;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Proxy;
 
 public class FieldInitialiserForPageElements implements FieldInitialiser {
     @Inject
-    private DependencyInjector dependencyInjector=null;
+    private DependencyInjector dependencyInjector = null;
     @Inject
-    private WebDriverFrameSwitchingOrchestrator webDriverFrameSwitchingOrchestrator=null;
+    private WebDriverFrameSwitchingOrchestrator webDriverFrameSwitchingOrchestrator = null;
 
-    public FieldInitialiserForPageElements(){
+    public FieldInitialiserForPageElements() {
     }
-    public Boolean initialiseField(Field field, Object page, SearchContext searchContext, FrameWeapper frame){
-        if (!FieldAssessor.isValidPageElement(field)){
+
+    public Boolean initialiseField(Field field, Object page, SearchContext searchContext, FrameWrapper frame) {
+        if (!FieldAssessor.isValidPageElement(field)) {
             return false;
-        }else {
-            Annotations annotations=new Annotations(field);
+        } else {
+            Annotations annotations = new Annotations(field);
+
             try {
-                PageElement pageElementProxy=this.getpageElementProxy(this.dependencyInjector, annotations.buildBy(), searchContext,frame,this.webDriverFrameSwitchingOrchestrator);
-                    field.setAccessible(true);
-                    field.set(page, pageElementProxy);
-                } catch (IllegalAccessException var7) {
-                 var7.printStackTrace();
+                PageElement pageElementProxy = this.getPageElementProxy(this.dependencyInjector, annotations.buildBy(), searchContext, frame, this.webDriverFrameSwitchingOrchestrator);
+                field.setAccessible(true);
+                field.set(page, pageElementProxy);
+            } catch (IllegalAccessException var7) {
+                var7.printStackTrace();
             }
+
             return true;
         }
     }
-    private PageElement getpageElementProxy(DependencyInjector driver, By by, SearchContext searchContext, FrameWeapper frame, WebDriverFrameSwitchingOrchestrator webDriverFrameSwitchingOrchestrator){
-        WebElementHandler elementHandler=new WebElementHandler(driver, searchContext, by, frame, webDriverFrameSwitchingOrchestrator);
-        WebElement proxyElement= (WebElement) Proxy.newProxyInstance(WebElement.class.getClassLoader(), new Class[]{WebElement.class, SearchContext.class, WrapsElement.class}, elementHandler);
+
+    private PageElement getPageElementProxy(DependencyInjector driver, By by, SearchContext searchContext, FrameWrapper frame, WebDriverFrameSwitchingOrchestrator webDriverFrameSwitchingOrchestrator) {
+        WebElementHandler elementHandler = new WebElementHandler(driver, searchContext, by, frame, webDriverFrameSwitchingOrchestrator);
+        WebElement proxyElement = (WebElement)Proxy.newProxyInstance(WebElement.class.getClassLoader(), new Class[]{WebElement.class, SearchContext.class, WrapsElement.class}, elementHandler);
         return new PageElementImpl(proxyElement);
     }
 }
