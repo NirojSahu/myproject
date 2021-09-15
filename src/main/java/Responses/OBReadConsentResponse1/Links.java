ad5a0de1254568c2acc8580ab7702e82bcb769d7
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:01:27 +0530

    Update Links.java

--- src/main/java/Responses/OBReadConsentResponse1/Links.java
+++ src/main/java/Responses/OBReadConsentResponse1/Links.java
@@ -1,36 +1,23 @@
-
 package Responses.OBReadConsentResponse1;
+
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.annotation.Generated;
+public class Links
+    {
+        @JsonProperty("Self")
+        private String self;
 
-
-
-public class Links {
-
-    @JsonProperty("Self")
-    private String mSelf;
-
-    public String getSelf() {
-        return mSelf;
-    }
-
-    public static class Builder {
-
-        private String mSelf;
-
-        public Links.Builder withSelf(String self) {
-            mSelf = self;
-            return this;
+        public String getSelf ()
+        {
+            return self;
         }
 
-        public Links build() {
-            Links links = new Links();
-            links.mSelf = mSelf;
-            return links;
-        }
-
+       /* @Override
+        public String toString()
+        {
+            return "ClassPojo [Self = "+Self+"]";
+        }*/
     }
 
-}
+
