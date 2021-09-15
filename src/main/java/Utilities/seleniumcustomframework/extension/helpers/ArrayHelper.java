6052eb134610d9c8d969b043f6f4b57aa3d97b6b
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:23:20 +0530

    Update ArrayHelper.java

--- src/main/java/Utilities/seleniumcustomframework/extension/helpers/ArrayHelper.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/helpers/ArrayHelper.java
@@ -1,23 +1,31 @@
-package Utilities.seleniumcustomframework.extension.helpers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import java.util.ArrayList;
+package com.test.seleniumcustomframework.extension.helpers;
+
 import java.util.Iterator;
 
 public class ArrayHelper {
-    private ArrayHelper(){
+    private ArrayHelper() {
     }
-    public static <T> String join(Iterable<T> aArr, String aSep) {
+
+    public static <T> String join(Iterable<T> aArr, String sSep) {
         StringBuilder sbStr = new StringBuilder();
         boolean first = true;
-        Iterator var4=aArr.iterator();
-        while (var4.hasNext()){
-            T value= (T) var4.next();
-            if (!first){
-                sbStr.append(aSep);
+        Iterator var4 = aArr.iterator();
+
+        while(var4.hasNext()) {
+            T value = var4.next();
+            if (!first) {
+                sbStr.append(sSep);
             }
-            first=false;
+
+            first = false;
             sbStr.append(value.toString());
         }
+
         return sbStr.toString();
     }
 }
