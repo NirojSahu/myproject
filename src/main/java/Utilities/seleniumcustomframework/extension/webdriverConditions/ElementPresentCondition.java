7761f7032037d17514de34347254cf6be93999ae
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:28:56 +0530

    Update ElementPresentCondition.java

--- src/main/java/Utilities/seleniumcustomframework/extension/webdriverConditions/ElementPresentCondition.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/webdriverConditions/ElementPresentCondition.java
@@ -1,27 +1,29 @@
-package Utilities.seleniumcustomframework.extension.webdriverConditions;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
+package com.test.seleniumcustomframework.extension.webdriverConditions;
+
+import javax.annotation.Nullable;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import javax.annotation.Nullable;
-import java.util.function.Function;
 
-public class ElementPresentCondition implements ExpectedCondition<WebElement>{
+public class ElementPresentCondition implements ExpectedCondition<WebElement> {
     private WebElement element;
 
-    public ElementPresentCondition() {
+    public ElementPresentCondition(WebElement element) {
+        this.element = element;
     }
 
-    public ElementPresentCondition(WebElement element) {
-        this.element=element;
-    }
     @Nullable
     public WebElement apply(@Nullable WebDriver webDriver) {
         try {
             this.element.getTagName();
             return this.element;
-        }catch (StaleElementReferenceException var3){
+        } catch (StaleElementReferenceException var3) {
             return null;
         }
     }
