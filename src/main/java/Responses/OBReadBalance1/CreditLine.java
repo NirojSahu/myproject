ce965832ba3967d384686e8362d1b3dbbfc94fef
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:55:03 +0530

    Update CreditLine.java

--- src/main/java/Responses/OBReadBalance1/CreditLine.java
+++ src/main/java/Responses/OBReadBalance1/CreditLine.java
@@ -1,61 +1,37 @@
-
 package Responses.OBReadBalance1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.annotation.Generated;
-
-
 public class CreditLine {
 
-    @JsonProperty("Amount")
-    private Amount mAmount;
-    @JsonProperty("Included")
-    private Boolean mIncluded;
     @JsonProperty("Type")
-    private String mType;
+    private String type;
 
-    public Amount getAmount() {
-        return mAmount;
+    @JsonProperty("Included")
+    private String included;
+
+    @JsonProperty("Amount")
+    private Amount amount;
+
+    public String getType ()
+    {
+        return type;
     }
 
-    public Boolean getIncluded() {
-        return mIncluded;
+    public String getIncluded ()
+    {
+        return included;
     }
 
-    public String getType() {
-        return mType;
+    public Amount getAmount ()
+    {
+        return amount;
     }
 
-    public static class Builder {
 
-        private Amount mAmount;
-        private Boolean mIncluded;
-        private String mType;
-
-        public CreditLine.Builder withAmount(Amount amount) {
-            mAmount = amount;
-            return this;
-        }
-
-        public CreditLine.Builder withIncluded(Boolean included) {
-            mIncluded = included;
-            return this;
-        }
-
-        public CreditLine.Builder withType(String type) {
-            mType = type;
-            return this;
-        }
-
-        public CreditLine build() {
-            CreditLine creditLine = new CreditLine();
-            creditLine.mAmount = mAmount;
-            creditLine.mIncluded = mIncluded;
-            creditLine.mType = mType;
-            return creditLine;
-        }
-
-    }
-
+    /*@Override
+    public String toString()
+    {
+        return "ClassPojo [Type = "+Type+", Included = "+Included+", Amount = "+Amount+"]";
+    }*/
 }
