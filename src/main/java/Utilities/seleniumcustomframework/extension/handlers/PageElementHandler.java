b0ff4b7177a613c1ffdfacd6a0885aab41a8390c
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:30:50 +0530

    Update PageElementHandler.java

--- src/main/java/Utilities/seleniumcustomframework/extension/handlers/PageElementHandler.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/handlers/PageElementHandler.java
@@ -1,26 +1,31 @@
-package Utilities.seleniumcustomframework.extension.handlers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.seleniumcustomframework.extension.PageElementImpl;
-import Utilities.seleniumcustomframework.extension.helpers.FrameWeapper;
-import Utilities.seleniumcustomframework.extension.orchestration.WebDriverFrameSwitchingOrchestrator;
+package com.test.seleniumcustomframework.extension.handlers;
 
+import com.test.seleniumcustomframework.extension.PageElementImpl;
+import com.test.seleniumcustomframework.extension.helpers.FrameWrapper;
+import com.test.seleniumcustomframework.extension.orchestration.WebDriverFrameSwitchingOrchestrator;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
-public class PageElementHandler {
+class PageElementHandler {
     private static final Logger LOG = Logger.getLogger(PageElementHandler.class.getName());
     private PageElementImpl pageElement;
-    private FrameWeapper frame;
+    private FrameWrapper frame;
     private WebDriverFrameSwitchingOrchestrator webDriverOrchestrator;
 
-    PageElementHandler(){
+    PageElementHandler() {
     }
+
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         try {
             this.webDriverOrchestrator.useFrame(this.frame);
-            return this.pageElement.canHandle(method) ? method.invoke(this.pageElement,args):  method.invoke(proxy,args);
-        }catch (InvocationTargetException var5){
+            return this.pageElement.canHandle(method) ? method.invoke(this.pageElement, args) : method.invoke(proxy, args);
+        } catch (InvocationTargetException var5) {
             throw var5.getCause();
         }
     }
