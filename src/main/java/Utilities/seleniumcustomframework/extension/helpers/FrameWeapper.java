d5a6145b30a469870cc2cd475f4afa95cbe5d95d
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:24:28 +0530

    Update FrameWeapper.java

--- src/main/java/Utilities/seleniumcustomframework/extension/helpers/FrameWeapper.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/helpers/FrameWeapper.java
@@ -1,66 +1,76 @@
-package Utilities.seleniumcustomframework.extension.helpers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.seleniumcustomframework.extension.annotations.Frame;
+package com.test.seleniumcustomframework.extension.helpers;
+
+import com.test.seleniumcustomframework.extension.annotations.Frame;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class FrameWeapper {
-    private static final Logger LOG=Logger.getLogger(Frame.class.getName());
+public class FrameWrapper {
+    private static final Logger LOG = Logger.getLogger(Frame.class.getName());
     private WebDriver driver;
-    private FrameWeapper parent;
+    private FrameWrapper parent;
     public By frameBy;
 
-    public FrameWeapper(WebDriver driver,By frameBy){
-        this.driver=driver;
-        this.frameBy=frameBy;
+    public FrameWrapper(WebDriver driver, By frameBy) {
+        this.driver = driver;
+        this.frameBy = frameBy;
     }
-    public boolean equals(Object o){
-        if (this==o){
+
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
-        }else if (!(o instanceof FrameWeapper)){
+        } else if (!(o instanceof FrameWrapper)) {
             return false;
-        }else {
-            FrameWeapper that= (FrameWeapper) o;
-            return that.hashCode()==that.hashCode();
+        } else {
+            FrameWrapper that = (FrameWrapper)o;
+            return this.hashCode() == that.hashCode();
         }
     }
-    public int hashCode(){
-        if (this.frameBy==null){
+
+    public int hashCode() {
+        if (this.frameBy == null) {
             return 0;
-        }else {
-            int result=this.frameBy.toString().hashCode();
-            if (this.parent !=null){
-                result +=this.parent.hashCode();
+        } else {
+            int result = this.frameBy.toString().hashCode();
+            if (this.parent != null) {
+                result += this.parent.hashCode();
             }
 
-            result =31* result;
+            result = 31 * result;
             return result;
         }
     }
-    public void use(){
-        if(this.parent!=null){
+
+    public void use() {
+        if (this.parent != null) {
             this.parent.use();
         }
-        LOG.log(Level.FINE, "Swetching to frame {0}", this);
-        WebElement frameElement=this.driver.findElement(this.frameBy);
+
+        LOG.log(Level.FINE, "Switching to frame {0}", this);
+        WebElement frameElement = this.driver.findElement(this.frameBy);
         this.driver.switchTo().frame(frameElement);
     }
-    public FrameWeapper setParent(FrameWeapper parent){
-        this.parent=parent;
+
+    public FrameWrapper setParent(FrameWrapper parent) {
+        this.parent = parent;
         return this;
     }
 
-    public String toString(){
-        ArrayList<String> frameWrapper =new ArrayList<>();
-        for (FrameWeapper frame=this; frame !=null; frame=frame.parent){
-            frameWrapper.add(frame.frameBy.toString());
-        }
-        return ArrayHelper.join(frameWrapper,"Inside ->");
-    }
+    public String toString() {
+        ArrayList<String> frameWrappers = new ArrayList();
 
+        for(FrameWrapper frame = this; frame != null; frame = frame.parent) {
+            frameWrappers.add(frame.frameBy.toString());
+        }
+
+        return ArrayHelper.join(frameWrappers, " inside -> ");
+    }
 }
