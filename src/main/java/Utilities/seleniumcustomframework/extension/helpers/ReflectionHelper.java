0e10a2481408bc75ae44f7d13bf468ac888ac15b
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:25:52 +0530

    Update ReflectionHelper.java

--- src/main/java/Utilities/seleniumcustomframework/extension/helpers/ReflectionHelper.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/helpers/ReflectionHelper.java
@@ -1,52 +1,68 @@
-package Utilities.seleniumcustomframework.extension.helpers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.seleniumcustomframework.extension.helpers;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ReflectionHelper {
-    private ReflectionHelper(){
+    private ReflectionHelper() {
     }
 
-    public static Object getFieldValue(Object object, String fieldName){
-        Field field=getField(object, fieldName);
-        if (field==null){
+    public static Object getFieldValue(Object object, String fieldName) {
+        Field field = getField(object, fieldName);
+        if (field == null) {
             return null;
-        }else {
+        } else {
             field.setAccessible(true);
-        }
-        try {
-            return field.get(object);
-        }catch (IllegalAccessException var4){
-            return null;
+
+            try {
+                return field.get(object);
+            } catch (IllegalAccessException var4) {
+                return null;
+            }
         }
     }
-    public static List<Field> getAllFields(Object object){
+
+    public static List<Field> getAllFields(Object object) {
         return getAllClassFields(object.getClass());
     }
 
-    public static List<Field> getAllClassFields(Class klass){
+    public static List<Field> getAllClassFields(Class klass) {
         ArrayList fields;
-        for (fields=new ArrayList(); klass!=null; klass=klass.getSuperclass()){
+        for(fields = new ArrayList(); klass != null; klass = klass.getSuperclass()) {
             Collections.addAll(fields, klass.getDeclaredFields());
         }
+
         return fields;
     }
-    public static Field getField(Class klass, String fieldName){
-        while (klass != null){
+
+    public static Field getField(Class klass, String fieldName) {
+        while(klass != null) {
             try {
                 Field field = klass.getDeclaredField(fieldName);
+                return field;
             } catch (NoSuchFieldException var3) {
-                klass=klass.getSuperclass();
+                klass = klass.getSuperclass();
             }
         }
+
         return null;
     }
-    public static boolean isWrapper(Class<?> clazz){
+
+    public static boolean isWrapperType(Class<?> clazz) {
         return getWrapperTypes().contains(clazz);
     }
 
-    private static Set<Class<?>> getWrapperTypes(){
-        Set<Class<?>> ret=new HashSet();
+    private static Set<Class<?>> getWrapperTypes() {
+        Set<Class<?>> ret = new HashSet();
         ret.add(Boolean.class);
         ret.add(Character.class);
         ret.add(Byte.class);
@@ -58,7 +74,8 @@ public class ReflectionHelper {
         ret.add(Void.class);
         return ret;
     }
-    private static Field getField(Object object, String fieldName){
+
+    private static Field getField(Object object, String fieldName) {
         return getField(object.getClass(), fieldName);
     }
 }
