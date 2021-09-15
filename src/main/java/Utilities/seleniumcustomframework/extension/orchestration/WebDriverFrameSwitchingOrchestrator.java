027a03693e4ece15c277e837cb98a0528fc81a6c
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:27:54 +0530

    Update WebDriverFrameSwitchingOrchestrator.java

--- src/main/java/Utilities/seleniumcustomframework/extension/orchestration/WebDriverFrameSwitchingOrchestrator.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/orchestration/WebDriverFrameSwitchingOrchestrator.java
@@ -1,42 +1,50 @@
-package Utilities.seleniumcustomframework.extension.orchestration;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.seleniumcustomframework.extension.dependencies.DependencyInjector;
-import Utilities.seleniumcustomframework.extension.helpers.FrameWeapper;
+package com.test.seleniumcustomframework.extension.orchestration;
+
 import com.google.inject.Inject;
-import groovy.lang.Singleton;
-import org.openqa.selenium.WebDriver;
-
+import com.google.inject.Singleton;
+import com.test.seleniumcustomframework.extension.dependencies.DependencyInjector;
+import com.test.seleniumcustomframework.extension.helpers.FrameWrapper;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.openqa.selenium.WebDriver;
 
 @Singleton
 public class WebDriverFrameSwitchingOrchestrator {
-    private static final Logger LOG=Logger.getLogger(WebDriverFrameSwitchingOrchestrator.class.getName());
-    private FrameWeapper frame;
+    private static final Logger LOG = Logger.getLogger(WebDriverFrameSwitchingOrchestrator.class.getName());
+    private FrameWrapper frame;
     @Inject
-    private DependencyInjector dependencyInjector=null;
+    private DependencyInjector dependencyInjector = null;
 
-    public WebDriverFrameSwitchingOrchestrator(){
+    public WebDriverFrameSwitchingOrchestrator() {
     }
 
-    public void useFrame(FrameWeapper frame){
-        LOG.log(Level.FINE,"Called use frame with {0}", frame);
-        if (frame==null){
+    public void useFrame(FrameWrapper frame) {
+        LOG.log(Level.FINE, "Called use frame with {0}", frame);
+        if (frame == null) {
             this.useDefault();
-        }else if (this.frame==null || !this.frame.equals(frame)){
+        } else if (this.frame == null || !this.frame.equals(frame)) {
             this.useDefault(true);
-            this.frame=frame;
+            this.frame = frame;
             frame.use();
         }
+
     }
-    private void useDefault(){
+
+    private void useDefault() {
         this.useDefault(false);
     }
-    private void useDefault(boolean force){
-        if (force || this.frame !=null){
-            this.frame=null;
-            LOG.log(Level.FINE,"Switching to default content");
+
+    private void useDefault(boolean force) {
+        if (force || this.frame != null) {
+            this.frame = null;
+            LOG.log(Level.FINE, "Switching to default content");
             ((WebDriver)this.dependencyInjector.get(WebDriver.class)).switchTo().defaultContent();
         }
+
     }
 }
