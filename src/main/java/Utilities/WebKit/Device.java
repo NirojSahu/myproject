f067a8fd48b0ef5ca4f702c5e76801f68c240cb1
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:46:24 +0530

    Create Device.java

---
+++ src/main/java/Utilities/WebKit/Device.java
@@ -0,0 +1,31 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.context;
+
+import cucumber.runtime.java.guice.ScenarioScoped;
+
+@ScenarioScoped
+public class Device {
+    public String type;
+
+    public Device() {
+    }
+
+    public Device getDeviceType(String deviceType) {
+        Device device = new Device();
+        device.type = deviceType.equalsIgnoreCase(Device.Type.Mobile.toString()) ? Device.Type.Mobile.toString() : Device.Type.Desktop.toString();
+        return device;
+    }
+
+    public static enum Type {
+        Mobile,
+        Desktop,
+        API;
+
+        private Type() {
+        }
+    }
+}
