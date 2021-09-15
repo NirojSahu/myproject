226b6a0a2b92575b15c695e0594a532a914af914
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:23:11 +0530

    Update DependencyInjector.java

--- src/main/java/Utilities/seleniumcustomframework/extension/dependencies/DependencyInjector.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/dependencies/DependencyInjector.java
@@ -1,4 +1,9 @@
-package Utilities.seleniumcustomframework.extension.dependencies;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.seleniumcustomframework.extension.dependencies;
 
 public interface DependencyInjector {
     <T> T get(Class<T> var1) throws InjectionError;
