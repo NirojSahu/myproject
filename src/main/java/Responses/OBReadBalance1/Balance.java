f47501af92205db35142552bc38447fa6e4e327c
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:55:26 +0530

    Update Balance.java

--- src/main/java/Responses/OBReadBalance1/Balance.java
+++ src/main/java/Responses/OBReadBalance1/Balance.java
@@ -1,102 +1,61 @@
-
 package Responses.OBReadBalance1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import javax.annotation.Generated;
-
-
 
 public class Balance {
 
-    @JsonProperty("AccountId")
-    private String mAccountId;
-    @JsonProperty("Amount")
-    private Amount mAmount;
-    @JsonProperty("CreditDebitIndicator")
-    private String mCreditDebitIndicator;
-    @JsonProperty("CreditLine")
-    private List<CreditLine> mCreditLine;
-    @JsonProperty("DateTime")
-    private String mDateTime;
     @JsonProperty("Type")
-    private String mType;
+    private String type;
 
-    public String getAccountId() {
-        return mAccountId;
+    @JsonProperty("AccountId")
+    private String accountId;
+
+    @JsonProperty("CreditLine")
+    private List<CreditLine> creditLine;
+
+    @JsonProperty("Amount")
+    private Amount amount;
+
+    @JsonProperty("CreditDebitIndicator")
+    private String creditDebitIndicator;
+
+    @JsonProperty("DateTime")
+    private String dateTime;
+
+    public String getType ()
+    {
+        return type;
     }
 
-    public Amount getAmount() {
-        return mAmount;
+    public String getAccountId ()
+    {
+        return accountId;
     }
 
-    public String getCreditDebitIndicator() {
-        return mCreditDebitIndicator;
+    public List<CreditLine> getCreditLine ()
+    {
+        return creditLine;
+    }
+    public Amount getAmount ()
+    {
+        return amount;
     }
 
-    public List<CreditLine> getCreditLine() {
-        return mCreditLine;
+    public String getCreditDebitIndicator ()
+    {
+        return creditDebitIndicator;
     }
 
-    public String getDateTime() {
-        return mDateTime;
+    public String getDateTime ()
+    {
+        return dateTime;
     }
 
-    public String getType() {
-        return mType;
-    }
-
-    public static class Builder {
-
-        private String mAccountId;
-        private Amount mAmount;
-        private String mCreditDebitIndicator;
-        private List<CreditLine> mCreditLine;
-        private String mDateTime;
-        private String mType;
-
-        public Balance.Builder withAccountId(String accountId) {
-            mAccountId = accountId;
-            return this;
-        }
-
-        public Balance.Builder withAmount(Amount amount) {
-            mAmount = amount;
-            return this;
-        }
-
-        public Balance.Builder withCreditDebitIndicator(String creditDebitIndicator) {
-            mCreditDebitIndicator = creditDebitIndicator;
-            return this;
-        }
-
-        public Balance.Builder withCreditLine(List<CreditLine> creditLine) {
-            mCreditLine = creditLine;
-            return this;
-        }
-
-        public Balance.Builder withDateTime(String dateTime) {
-            mDateTime = dateTime;
-            return this;
-        }
-
-        public Balance.Builder withType(String type) {
-            mType = type;
-            return this;
-        }
-
-        public Balance build() {
-            Balance balance = new Balance();
-            balance.mAccountId = mAccountId;
-            balance.mAmount = mAmount;
-            balance.mCreditDebitIndicator = mCreditDebitIndicator;
-            balance.mCreditLine = mCreditLine;
-            balance.mDateTime = mDateTime;
-            balance.mType = mType;
-            return balance;
-        }
-
-    }
-
+    /*@Override
+    public String toString()
+    {
+        return "ClassPojo [Type = "+Type+", AccountId = "+AccountId+", CreditLine = "+CreditLine+", Amount = "+Amount+", CreditDebitIndicator = "+CreditDebitIndicator+", DateTime = "+DateTime+"]";
+    }*/
 }
