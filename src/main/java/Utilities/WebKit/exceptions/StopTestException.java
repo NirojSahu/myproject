3afd6849b5a4403dbce4f5702612f17e51e691ca
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:44:32 +0530

    Create StopTestException.java

---
+++ src/main/java/Utilities/WebKit/exceptions/StopTestException.java
@@ -0,0 +1,39 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.exceptions;
+
+import com.test.helper.ExecControlHelper;
+
+public class StopTestException extends Exception {
+    private static final long serialVersionUID = 1L;
+    private String screenshot;
+
+    public StopTestException(String message, boolean screenshot) {
+        this.initCause(new Throwable(message));
+    }
+
+    public StopTestException(String message, Throwable exception) {
+        super(message, exception);
+        ExecControlHelper.stopTest(message);
+    }
+
+    public StopTestException(String message) {
+        super(message);
+        ExecControlHelper.stopTest(message);
+    }
+
+    public StopTestException(String messageFormat, Object... arguments) {
+        this(String.format(messageFormat, arguments));
+    }
+
+    public String getScreenshot() {
+        return this.screenshot;
+    }
+
+    public void setScreenshot(String screenshot) {
+        this.screenshot = screenshot;
+    }
+}
