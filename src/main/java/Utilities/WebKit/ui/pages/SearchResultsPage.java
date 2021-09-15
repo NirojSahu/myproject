f729b0fd8919797870077e83ab5144d456585745
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:04:15 +0530

    Update SearchResultsPage.java

--- src/main/java/Utilities/WebKit/ui/pages/SearchResultsPage.java
+++ src/main/java/Utilities/WebKit/ui/pages/SearchResultsPage.java
@@ -1,18 +1,23 @@
-package Utilities.WebKit.ui.pages;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.WebKit.ui.pageSelections.SearchResults;
-import Utilities.seleniumcustomframework.extension.PageElement;
-import Utilities.seleniumcustomframework.extension.annotations.Section;
+package com.test.ui.selenium.pages;
+
+import com.test.seleniumcustomframework.extension.PageElement;
+import com.test.seleniumcustomframework.extension.annotations.Section;
+import com.test.ui.selenium.pageSections.SearchResults;
 import org.openqa.selenium.support.FindBy;
 
 public class SearchResultsPage {
-    public PageElement resultstates;
+    public PageElement resultstats;
     @Section
     @FindBy(
-            id="center_col"
+        id = "center_col"
     )
     public SearchResults searchResults;
-    public SearchResultsPage(){
 
+    public SearchResultsPage() {
     }
 }
