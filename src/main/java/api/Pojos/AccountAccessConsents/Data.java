f0568808344fb675de95ef5e08f8f7166f46cf1c
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:45:00 +0530

    Update Data.java

--- src/main/java/api/Pojos/AccountAccessConsents/Data.java
+++ src/main/java/api/Pojos/AccountAccessConsents/Data.java
@@ -1,73 +1,66 @@
-
 package api.Pojos.AccountAccessConsents;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import javax.annotation.Generated;
-public class Data {
+
+public class Data
+{
+    @JsonProperty("Permissions")
+    private List<String> permissions;
+
+    @JsonProperty("TransactionToDateTime")
+    private String transactionToDateTime;
 
     @JsonProperty("ExpirationDateTime")
-    private String mExpirationDateTime;
-    @JsonProperty("Permissions")
-    private List<String> mPermissions;
+    private String expirationDateTime;
+
     @JsonProperty("TransactionFromDateTime")
-    private String mTransactionFromDateTime;
-    @JsonProperty("TransactionToDateTime")
-    private String mTransactionToDateTime;
+    private String transactionFromDateTime;
 
-    public String getExpirationDateTime() {
-        return mExpirationDateTime;
+    public String getTransactionToDateTime ()
+    {
+        return transactionToDateTime;
     }
 
-    public List<String> getPermissions() {
-        return mPermissions;
+    public void setTransactionToDateTime (String transactionToDateTime)
+    {
+        this.transactionToDateTime = transactionToDateTime;
     }
 
-    public String getTransactionFromDateTime() {
-        return mTransactionFromDateTime;
+    public String getExpirationDateTime ()
+    {
+        return expirationDateTime;
     }
 
-    public String getTransactionToDateTime() {
-        return mTransactionToDateTime;
+    public void setExpirationDateTime (String expirationDateTime)
+    {
+        this.expirationDateTime = expirationDateTime;
     }
 
-    public static class Builder {
-
-        private String mExpirationDateTime;
-        private List<String> mPermissions;
-        private String mTransactionFromDateTime;
-        private String mTransactionToDateTime;
-
-        public Data.Builder withExpirationDateTime(String expirationDateTime) {
-            mExpirationDateTime = expirationDateTime;
-            return this;
-        }
-
-        public Data.Builder withPermissions(List<String> permissions) {
-            mPermissions = permissions;
-            return this;
-        }
-
-        public Data.Builder withTransactionFromDateTime(String transactionFromDateTime) {
-            mTransactionFromDateTime = transactionFromDateTime;
-            return this;
-        }
-
-        public Data.Builder withTransactionToDateTime(String transactionToDateTime) {
-            mTransactionToDateTime = transactionToDateTime;
-            return this;
-        }
-
-        public Data build() {
-            Data data = new Data();
-            data.mExpirationDateTime = mExpirationDateTime;
-            data.mPermissions = mPermissions;
-            data.mTransactionFromDateTime = mTransactionFromDateTime;
-            data.mTransactionToDateTime = mTransactionToDateTime;
-            return data;
-        }
-
+    public List<String> getPermissions ()
+    {
+        return permissions;
     }
 
+    public void setPermissions (List<String> permissions)
+    {
+        this.permissions = permissions;
+    }
+
+    public String getTransactionFromDateTime ()
+    {
+        return transactionFromDateTime;
+    }
+
+    public void setTransactionFromDateTime (String transactionFromDateTime)
+    {
+        this.transactionFromDateTime = transactionFromDateTime;
+    }
+
+  /*  @Override
+    public String toString()
+    {
+        return "ClassPojo [TransactionToDateTime = "+TransactionToDateTime+", ExpirationDateTime = "+ExpirationDateTime+", Permissions = "+Permissions+", TransactionFromDateTime = "+TransactionFromDateTime+"]";
+    }*/
 }
