18814ff1a5ec1592cd52ea6b84bce4ab09ed2023
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:26:14 +0530

    Update SortingHelper.java

--- src/main/java/Utilities/seleniumcustomframework/extension/helpers/SortingHelper.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/helpers/SortingHelper.java
@@ -1,14 +1,23 @@
-package Utilities.seleniumcustomframework.extension.helpers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import java.util.*;
+package com.test.seleniumcustomframework.extension.helpers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class SortingHelper {
-    private SortingHelper(){
+    private SortingHelper() {
     }
 
-    public static <T> List<T> asSortedList(Collection<T> c, Comparator<T> comparator){
-        List<T> list= new ArrayList(c);
-        Collections.sort(list,comparator);
+    public static <T> List<T> asSortedList(Collection<T> c, Comparator<T> comparator) {
+        List<T> list = new ArrayList(c);
+        Collections.sort(list, comparator);
         return list;
     }
 }
