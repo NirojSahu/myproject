8f0cf86f7a29b85008474dd0eb8269a4d1d1095d
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:44:12 +0530

    Create TestDataError.java

---
+++ src/main/java/Utilities/WebKit/exceptions/TestDataError.java
@@ -0,0 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.exceptions;
+
+public class TestDataError extends RuntimeException {
+    public TestDataError(String message) {
+        super(message);
+    }
+}
