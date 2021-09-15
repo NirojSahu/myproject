9b83d09c477a9f668e173403f76c818ff979d8c2
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:41:44 +0530

    Update CertJKSUtility.java

--- src/main/java/Utilities/CertJKSUtility.java
+++ src/main/java/Utilities/CertJKSUtility.java
@@ -4,62 +4,71 @@ import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
 import io.restassured.specification.RequestSpecification;
-
-//import javax.net.ssl.SSLSocketFactory;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.security.*;
-
 import org.apache.http.conn.ssl.SSLSocketFactory;
 
+import java.io.FileInputStream;
+import java.security.*;
+
 public class CertJKSUtility {
-    public static RequestSpecification clintCertSpecification(
-            String keyStorePath, String keyStorePass) throws UnrecoverableKeyException, FileNotFoundException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        return clintCertSpecification(keyStorePath, keyStorePass,null,null);
+
+    public static RequestSpecification clientCertSpecification(
+            String keyStorePath, String keyStorePass)
+            throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException,
+            KeyManagementException {
+        return clientCertSpecification(keyStorePath, keyStorePass, null, null);
     }
 
-
-    public static RequestSpecification clintCertSpecification(
-            String kyeStorePath, String keyStorePass, String trustStorePath, String trustStorePass   ) throws UnrecoverableKeyException, FileNotFoundException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        return clintCertSpecification(
-                kyeStorePath,
+    public static RequestSpecification clientCertSpecification(
+            String keyStorePath, String keyStorePass, String trustStorePath, String trustStorePass)
+            throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException,
+            KeyManagementException {
+        return clientCertSpecification(
+                keyStorePath,
                 keyStorePass,
                 KeyStore.getDefaultType(),
                 trustStorePath,
                 trustStorePass,
-                KeyStore.getDefaultType()
-                );
+                KeyStore.getDefaultType());
     }
+
     @Deprecated
-    public static RequestSpecification clintCertSpecification(String keyStorePath,
-                                                              String keyStorePass,
-                                                              String keyStoreType,
-                                                              String trustStorePath,
-                                                              String trustStorePass,
-                                                              String trustStoreType) throws FileNotFoundException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        KeyStore keyStore=loadKeyStore(keyStorePath,keyStorePass.toCharArray(),keyStoreType);
-        SSLSocketFactory clintAuthFactory=new SSLSocketFactory(keyStore,keyStorePass);
-        if(null !=trustStorePath){
-            KeyStore trustStore=
-                    loadKeyStore(keyStorePath,trustStorePass.toCharArray(),trustStoreType);
-            clintAuthFactory =new SSLSocketFactory(keyStore,keyStorePass,trustStore);
+    public static RequestSpecification clientCertSpecification(
+            String keyStorePath,
+            String keyStorePass,
+            String keyStoreType,
+            String trustStorePath,
+            String trustStorePass,
+            String trustStoreType)
+            throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException,
+            KeyManagementException {
+
+        KeyStore keyStore = loadKeyStore(keyStorePath, keyStorePass.toCharArray(), keyStoreType);
+        SSLSocketFactory clientAuthFactory = new SSLSocketFactory(keyStore, keyStorePass);
+        if (null != trustStorePath) {
+            KeyStore trustStore =
+                    loadKeyStore(trustStorePath, trustStorePass.toCharArray(), trustStoreType);
+            clientAuthFactory = new SSLSocketFactory(keyStore, keyStorePass, trustStore);
         }
-        SSLConfig sslConfig=
-                RestAssuredConfig.config().getSSLConfig().with().sslSocketFactory(clintAuthFactory);
-        RestAssuredConfig config= RestAssured.config().sslConfig(sslConfig);
+
+        SSLConfig sslConfig =
+                RestAssuredConfig.config().getSSLConfig().with().sslSocketFactory(clientAuthFactory);
+        RestAssuredConfig config = RestAssured.config().sslConfig(sslConfig);
+
         return RestAssured.given().config(config);
     }
-    public static KeyStore loadKeyStore(String path, char[] password,String storeType) throws FileNotFoundException {
+
+    public static KeyStore loadKeyStore(String path, char[] password, String storeType) {
         KeyStore keyStore;
-        try{
-            keyStore=KeyStore.getInstance(storeType);
-            keyStore.load(new FileInputStream(path),password);
-        }catch (Exception ex){
-            throw new RuntimeException("Error While Executing the KyeStore:", ex);
+        try {
+            keyStore = KeyStore.getInstance(storeType);
+            keyStore.load(new FileInputStream(path), password);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error while extracting the keystore", ex);
         }
         return keyStore;
     }
 
-
-
+    public static KeyStore loadKeyStore(String path, char[] password) {
+        return loadKeyStore(path, password, KeyStore.getDefaultType());
+    }
 }
