144f64732220094715cee4a447136b936a382a4a
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:02:53 +0530

    Update Data.java

--- src/main/java/Responses/OBReadDirectDebits1/Data.java
+++ src/main/java/Responses/OBReadDirectDebits1/Data.java
@@ -4,11 +4,10 @@ package Responses.OBReadDirectDebits1;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import javax.annotation.Generated;
-
 
 
 public class Data {
+
 
     @JsonProperty("DirectDebit")
     private List<DirectDebit> mDirectDebit;
@@ -17,21 +16,8 @@ public class Data {
         return mDirectDebit;
     }
 
-    public static class Builder {
-
-        private List<DirectDebit> mDirectDebit;
-
-        public Data.Builder withDirectDebit(List<DirectDebit> directDebit) {
-            mDirectDebit = directDebit;
-            return this;
-        }
-
-        public Data build() {
-            Data data = new Data();
-            data.mDirectDebit = mDirectDebit;
-            return data;
-        }
-
+    public void setDirectDebit(List<DirectDebit> directDebit) {
+        mDirectDebit = directDebit;
     }
 
 }
