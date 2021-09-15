c917a80583d2a9f312885723caee0403268f18aa
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:02:10 +0530

    Update OBReadConsentResponse.java

--- src/main/java/Responses/OBReadConsentResponse1/OBReadConsentResponse.java
+++ src/main/java/Responses/OBReadConsentResponse1/OBReadConsentResponse.java
@@ -1,74 +1,49 @@
-
 package Responses.OBReadConsentResponse1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.annotation.Generated;
+public class OBReadConsentResponse
+{
+    @JsonProperty("Meta")
+    private Meta meta;
 
+    /*@JsonProperty("Risk")
+    private String risk;*/
 
-public class OBReadConsentResponse {
+    @JsonProperty("Links")
+    private Links links;
 
     @JsonProperty("Data")
-    private Data mData;
-    @JsonProperty("Links")
-    private Links mLinks;
-    @JsonProperty("Meta")
-    private Meta mMeta;
+    private Data data;
+
     @JsonProperty("Risk")
-    private Risk mRisk;
+    public Object risk;
 
-    public Data getData() {
-        return mData;
+    public Object getRisk ()
+    {
+        return risk ;
     }
 
-    public Links getLinks() {
-        return mLinks;
+    public Meta getMeta ()
+    {
+        return meta;
     }
 
-    public Meta getMeta() {
-        return mMeta;
+    public Links getLinks ()
+    {
+        return links;
     }
 
-    public Risk getRisk() {
-        return mRisk;
+    public Data getData ()
+    {
+        return data;
     }
 
-    public static class Builder {
 
-        private Data mData;
-        private Links mLinks;
-        private Meta mMeta;
-        private Risk mRisk;
 
-        public OBReadConsentResponse.Builder withData(Data data) {
-            mData = data;
-            return this;
-        }
-
-        public OBReadConsentResponse.Builder withLinks(Links links) {
-            mLinks = links;
-            return this;
-        }
-
-        public OBReadConsentResponse.Builder withMeta(Meta meta) {
-            mMeta = meta;
-            return this;
-        }
-
-        public OBReadConsentResponse.Builder withRisk(Risk risk) {
-            mRisk = risk;
-            return this;
-        }
-
-        public OBReadConsentResponse build() {
-            OBReadConsentResponse oBReadConsentResponse = new OBReadConsentResponse();
-            oBReadConsentResponse.mData = mData;
-            oBReadConsentResponse.mLinks = mLinks;
-            oBReadConsentResponse.mMeta = mMeta;
-            oBReadConsentResponse.mRisk = mRisk;
-            return oBReadConsentResponse;
-        }
-
-    }
-
+   /* @Override
+    public String toString()
+    {
+        return "ClassPojo [Meta = "+Meta+", Risk = "+Risk+", Links = "+Links+", Data = "+Data+"]";
+    }*/
 }
