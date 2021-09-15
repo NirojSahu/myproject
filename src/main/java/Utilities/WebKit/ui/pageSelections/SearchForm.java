409887468647447ecf09df855e6ab2ac2e21aa5f
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:05:56 +0530

    Create SearchForm.java

---
+++ src/main/java/Utilities/WebKit/ui/pageSelections/SearchForm.java
@@ -0,0 +1,29 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.ui.selenium.pageSections;
+
+import com.google.inject.Inject;
+import com.test.seleniumcustomframework.extension.PageElement;
+import com.test.seleniumcustomframework.extension.PageSection;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchForm extends PageSection {
+    @Inject
+    WebDriver driver;
+    @FindBy(
+        css = "input[name='q']"
+    )
+    PageElement query;
+
+    public SearchForm() {
+    }
+
+    public void searchFor(String term) {
+        this.query.sendKeys(new CharSequence[]{term});
+        this.query.submit();
+    }
+}
