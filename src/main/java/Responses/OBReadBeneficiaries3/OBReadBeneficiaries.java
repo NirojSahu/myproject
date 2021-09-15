84698b7f28cf7c96634190dd7611990bf39fa112
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:00:29 +0530

    Update OBReadBeneficiaries.java

--- src/main/java/Responses/OBReadBeneficiaries3/OBReadBeneficiaries.java
+++ src/main/java/Responses/OBReadBeneficiaries3/OBReadBeneficiaries.java
@@ -2,17 +2,18 @@
 package Responses.OBReadBeneficiaries3;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.annotation.Generated;
-
+import com.google.gson.annotations.SerializedName;
 
 
 public class OBReadBeneficiaries {
 
+    @SerializedName("Data")
     @JsonProperty("Data")
     private Data mData;
+    @SerializedName("Links")
     @JsonProperty("Links")
     private Links mLinks;
+    @SerializedName("Meta")
     @JsonProperty("Meta")
     private Meta mMeta;
 
@@ -34,27 +35,27 @@ public class OBReadBeneficiaries {
         private Links mLinks;
         private Meta mMeta;
 
-        public OBReadBeneficiaries.Builder withData(Data data) {
+        public Builder withData(Data data) {
             mData = data;
             return this;
         }
 
-        public OBReadBeneficiaries.Builder withLinks(Links links) {
+        public Builder withLinks(Links links) {
             mLinks = links;
             return this;
         }
 
-        public OBReadBeneficiaries.Builder withMeta(Meta meta) {
+        public Builder withMeta(Meta meta) {
             mMeta = meta;
             return this;
         }
 
         public OBReadBeneficiaries build() {
-            OBReadBeneficiaries OBReadBeneficiaries = new OBReadBeneficiaries();
-            OBReadBeneficiaries.mData = mData;
-            OBReadBeneficiaries.mLinks = mLinks;
-            OBReadBeneficiaries.mMeta = mMeta;
-            return OBReadBeneficiaries;
+            OBReadBeneficiaries oBReadBeneficiaries = new OBReadBeneficiaries();
+            oBReadBeneficiaries.mData = mData;
+            oBReadBeneficiaries.mLinks = mLinks;
+            oBReadBeneficiaries.mMeta = mMeta;
+            return oBReadBeneficiaries;
         }
 
     }
