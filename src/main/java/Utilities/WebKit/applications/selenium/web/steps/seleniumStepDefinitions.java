ca9896e65c5a22e4fc4f3f7368ff576f3aa31cc5
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:12:54 +0530

    Update seleniumStepDefinitions.java

---
+++ src/main/java/Utilities/WebKit/applications/selenium/web/steps/seleniumStepDefinitions.java
@@ -0,0 +1,63 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.applications.selenium.web.steps;
+
+import com.google.inject.Inject;
+import com.test.helper.NavigationHelper;
+import com.test.ui.selenium.pageSections.SearchResult;
+import com.test.ui.selenium.pages.HomePage;
+import com.test.ui.selenium.pages.SearchResultsPage;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import java.util.Optional;
+import org.openqa.selenium.WebDriver;
+
+public class SeleniumStepDefinitions {
+    @Inject
+    HomePage homePage;
+    @Inject
+    SearchResultsPage searchResultsPage;
+    @Inject
+    NavigationHelper navigationHelper;
+    @Inject
+    WebDriver driver;
+
+    public SeleniumStepDefinitions() {
+    }
+
+    @Given("^I open a browser$")
+    public void I_open_a_browser_chrome() throws Throwable {
+        System.out.println("Starting browser \n");
+    }
+
+    @When("^I navigate to a Website$")
+    public void i_navigate_to_a_Website() throws Throwable {
+        System.out.println("Navigate to Google Home page \n");
+        this.navigationHelper.navigateTo("http://www.google.co.uk/");
+    }
+
+    @And("^I search for Selenium$")
+    public void I_add_a_product_to_basket() throws Throwable {
+        System.out.println("Searching for Selenium \n");
+        this.homePage.searchForm.searchFor("Selenium");
+        Thread.sleep(5000L);
+    }
+
+    @Then("^I should be able to click on desired result$")
+    public void I_should_go_to_basket_page() throws Throwable {
+        System.out.println("Clicking on desired result \n");
+        Optional<SearchResult> searchResult = this.searchResultsPage.searchResults.results.stream().filter((r) -> {
+            return r.getUrl().contains("seleniumhq.org");
+        }).findAny();
+        if (!searchResult.isPresent()) {
+            throw new RuntimeException("Could not find the url");
+        } else {
+            ((SearchResult)searchResult.get()).select();
+        }
+    }
+}
