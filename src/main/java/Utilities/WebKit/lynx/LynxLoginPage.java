5e9e74d1db1592104ec2ae906f88ffd5b5d709f2
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:02:00 +0530

    Create LynxLoginPage.java

---
+++ src/main/java/Utilities/WebKit/lynx/LynxLoginPage.java
@@ -0,0 +1,28 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.lynx;
+
+import com.test.helper.BasePage;
+import java.net.MalformedURLException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class LynxLoginPage extends BasePage {
+    public LynxLoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void toLynxHomePage() throws MalformedURLException {
+        this.driver.get(PropertyReader.getLynxUrl());
+        this.driver.findElement(By.name("sUsuarioForm")).sendKeys(new CharSequence[]{PropertyReader.getLynxUsername()});
+        this.driver.findElement(By.id("sClave")).sendKeys(new CharSequence[]{PropertyReader.getLynxPassword()});
+        WebElement element = this.driver.findElement(By.name("lAplicacion"));
+        (new Select(element)).selectByVisibleText(PropertyReader.getLynxSystype());
+        this.driver.findElement(By.id("LOGIN_BUTTON")).click();
+    }
+}
