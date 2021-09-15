277e5f4060971f573d5b03fa0d32482634afc091
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:43:41 +0530

    Update UnexpectedPageException.java

--- src/main/java/Utilities/WebKit/exceptions/UnexpectedPageException.java
+++ src/main/java/Utilities/WebKit/exceptions/UnexpectedPageException.java
@@ -1,7 +1,12 @@
-package Utilities.WebKit.exceptions;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.exceptions;
 
 public class UnexpectedPageException extends RuntimeException {
-    public UnexpectedPageException(String message){
+    public UnexpectedPageException(String message) {
         super(message);
     }
 }
