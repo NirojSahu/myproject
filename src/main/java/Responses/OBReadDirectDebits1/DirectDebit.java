c4aa4c5b982ce9cd9c40cece43ef86e81da68f1a
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:03:12 +0530

    Update DirectDebit.java

--- src/main/java/Responses/OBReadDirectDebits1/DirectDebit.java
+++ src/main/java/Responses/OBReadDirectDebits1/DirectDebit.java
@@ -3,11 +3,8 @@ package Responses.OBReadDirectDebits1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.annotation.Generated;
-
-
-
 public class DirectDebit {
+
 
     @JsonProperty("AccountId")
     private String mAccountId;
@@ -28,87 +25,56 @@ public class DirectDebit {
         return mAccountId;
     }
 
+    public void setAccountId(String accountId) {
+        mAccountId = accountId;
+    }
+
     public String getDirectDebitId() {
         return mDirectDebitId;
+    }
+
+    public void setDirectDebitId(String directDebitId) {
+        mDirectDebitId = directDebitId;
     }
 
     public String getDirectDebitStatusCode() {
         return mDirectDebitStatusCode;
     }
 
+    public void setDirectDebitStatusCode(String directDebitStatusCode) {
+        mDirectDebitStatusCode = directDebitStatusCode;
+    }
+
     public String getMandateIdentification() {
         return mMandateIdentification;
+    }
+
+    public void setMandateIdentification(String mandateIdentification) {
+        mMandateIdentification = mandateIdentification;
     }
 
     public String getName() {
         return mName;
     }
 
+    public void setName(String name) {
+        mName = name;
+    }
+
     public PreviousPaymentAmount getPreviousPaymentAmount() {
         return mPreviousPaymentAmount;
+    }
+
+    public void setPreviousPaymentAmount(PreviousPaymentAmount previousPaymentAmount) {
+        mPreviousPaymentAmount = previousPaymentAmount;
     }
 
     public String getPreviousPaymentDateTime() {
         return mPreviousPaymentDateTime;
     }
 
-    public static class Builder {
-
-        private String mAccountId;
-        private String mDirectDebitId;
-        private String mDirectDebitStatusCode;
-        private String mMandateIdentification;
-        private String mName;
-        private PreviousPaymentAmount mPreviousPaymentAmount;
-        private String mPreviousPaymentDateTime;
-
-        public DirectDebit.Builder withAccountId(String accountId) {
-            mAccountId = accountId;
-            return this;
-        }
-
-        public DirectDebit.Builder withDirectDebitId(String directDebitId) {
-            mDirectDebitId = directDebitId;
-            return this;
-        }
-
-        public DirectDebit.Builder withDirectDebitStatusCode(String directDebitStatusCode) {
-            mDirectDebitStatusCode = directDebitStatusCode;
-            return this;
-        }
-
-        public DirectDebit.Builder withMandateIdentification(String mandateIdentification) {
-            mMandateIdentification = mandateIdentification;
-            return this;
-        }
-
-        public DirectDebit.Builder withName(String name) {
-            mName = name;
-            return this;
-        }
-
-        public DirectDebit.Builder withPreviousPaymentAmount(PreviousPaymentAmount previousPaymentAmount) {
-            mPreviousPaymentAmount = previousPaymentAmount;
-            return this;
-        }
-
-        public DirectDebit.Builder withPreviousPaymentDateTime(String previousPaymentDateTime) {
-            mPreviousPaymentDateTime = previousPaymentDateTime;
-            return this;
-        }
-
-        public DirectDebit build() {
-            DirectDebit directDebit = new DirectDebit();
-            directDebit.mAccountId = mAccountId;
-            directDebit.mDirectDebitId = mDirectDebitId;
-            directDebit.mDirectDebitStatusCode = mDirectDebitStatusCode;
-            directDebit.mMandateIdentification = mMandateIdentification;
-            directDebit.mName = mName;
-            directDebit.mPreviousPaymentAmount = mPreviousPaymentAmount;
-            directDebit.mPreviousPaymentDateTime = mPreviousPaymentDateTime;
-            return directDebit;
-        }
-
+    public void setPreviousPaymentDateTime(String previousPaymentDateTime) {
+        mPreviousPaymentDateTime = previousPaymentDateTime;
     }
 
 }
