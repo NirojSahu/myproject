1f2a0b43a62db337e39931c889a6302b551002c6
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:59:30 +0530

    Update BrowserHooks.java

--- src/main/java/Utilities/WebKit/hooks/BrowserHooks.java
+++ src/main/java/Utilities/WebKit/hooks/BrowserHooks.java
@@ -1,4 +1,45 @@
-package Utilities.WebKit.hooks;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.hooks;
+
+import com.google.inject.Inject;
+import com.test.configuration.Configuration;
+import com.test.configuration.PageFactory;
+import com.test.exceptions.StopTestException;
+import com.test.helper.EnvironmentConstants;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import java.net.MalformedURLException;
+import org.openqa.selenium.WebDriver;
 
 public class BrowserHooks {
+    @Inject
+    public WebDriver driver;
+
+    public BrowserHooks() {
+    }
+
+    @Before
+    public void setup(Scenario scenario) throws StopTestException, MalformedURLException {
+        String browser = Configuration.getConfiguration().getSafeBrowser();
+    }
+
+    @Before
+    public void getScenario(Scenario scenario) {
+        String scenarioId = scenario.getName();
+        System.out.println("\n Scenario Name => " + scenarioId + "\n");
+        String[] parts = scenarioId.split(":");
+        EnvironmentConstants.tagId = parts[0];
+    }
+
+    @After(
+        order = 0
+    )
+    public void teardown(Scenario scenario) throws Exception {
+        PageFactory.tearDown();
+    }
 }
