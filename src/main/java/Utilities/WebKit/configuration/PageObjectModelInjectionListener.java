143a9f4d52a9a8202b1928b75dbb9a143254f502
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:24:56 +0530

    Create PageObjectModelInjectionListener.java

---
+++ src/main/java/Utilities/WebKit/configuration/PageObjectModelInjectionListener.java
@@ -0,0 +1,24 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.configuration;
+
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+import com.google.inject.spi.InjectionListener;
+import com.test.seleniumcustomframework.extension.PageFactory;
+
+public class PageObjectModelInjectionListener implements InjectionListener {
+    private Provider<Injector> injector;
+
+    public PageObjectModelInjectionListener(Provider<Injector> injector) {
+        this.injector = injector;
+    }
+
+    public void afterInjection(Object page) {
+        PageFactory pageFactory = (PageFactory)((Injector)this.injector.get()).getInstance(PageFactory.class);
+        pageFactory.get(page);
+    }
+}
