b79c6c310157b071764514abb7cb6124ec485357
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:04:37 +0530

    Create HomePage.java

---
+++ src/main/java/Utilities/WebKit/ui/pages/HomePage.java
@@ -0,0 +1,21 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.ui.selenium.pages;
+
+import com.test.seleniumcustomframework.extension.annotations.Section;
+import com.test.ui.selenium.pageSections.SearchForm;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+    @Section
+    @FindBy(
+        id = "sfdiv"
+    )
+    public SearchForm searchForm;
+
+    public HomePage() {
+    }
+}
