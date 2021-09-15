2e50dc173959352b3ab830f2bb140f7add35c4d2
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:24:13 +0530

    Update InjectionError.java

--- src/main/java/Utilities/seleniumcustomframework/extension/dependencies/InjectionError.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/dependencies/InjectionError.java
@@ -1,6 +1,16 @@
-package Utilities.seleniumcustomframework.extension.dependencies;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.seleniumcustomframework.extension.dependencies;
 
 public class InjectionError extends Error {
-    public InjectionError(Throwable cause){super(cause);}
-    public InjectionError(String message){super(message);}
+    public InjectionError(Throwable cause) {
+        super(cause);
+    }
+
+    public InjectionError(String message) {
+        super(message);
+    }
 }
