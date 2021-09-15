b51e6f1812ba2f0ef4d028f07729a93000f0c66d
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:55:51 +0530

    Update Amount.java

--- src/main/java/Responses/OBReadBalance1/Amount.java
+++ src/main/java/Responses/OBReadBalance1/Amount.java
@@ -1,44 +1,28 @@
-
 package Responses.OBReadBalance1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Amount {
 
     @JsonProperty("Amount")
-    private String mAmount;
+    private String amount;
+
     @JsonProperty("Currency")
-    private String mCurrency;
+    private String currency;
 
-    public String getAmount() {
-        return mAmount;
+    public String getAmount ()
+    {
+        return amount;
     }
 
-    public String getCurrency() {
-        return mCurrency;
+    public String getCurrency ()
+    {
+        return currency;
     }
 
-    public static class Builder {
-
-        private String mAmount;
-        private String mCurrency;
-
-        public Amount.Builder withAmount(String amount) {
-            mAmount = amount;
-            return this;
-        }
-
-        public Amount.Builder withCurrency(String currency) {
-            mCurrency = currency;
-            return this;
-        }
-
-        public Amount build() {
-            Amount amount = new Amount();
-            amount.mAmount = mAmount;
-            amount.mCurrency = mCurrency;
-            return amount;
-        }
-
-    }
-
+    /*@Override
+    public String toString()
+    {
+        return "ClassPojo [Amount = "+amount+", Currency = "+currency+"]";
+    }*/
 }
