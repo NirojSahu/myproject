a08dcfdcc35ec9d17e822df8fb845b2e271451ee
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 18:24:08 +0530

    Update FieldInitialiserSort.java

--- src/main/java/Utilities/seleniumcustomframework/extension/helpers/FieldInitialiserSort.java
+++ src/main/java/Utilities/seleniumcustomframework/extension/helpers/FieldInitialiserSort.java
@@ -1,13 +1,17 @@
-package Utilities.seleniumcustomframework.extension.helpers;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import Utilities.seleniumcustomframework.extension.fieldInitialisers.FieldInitialiser;
+package com.test.seleniumcustomframework.extension.helpers;
 
+import com.test.seleniumcustomframework.extension.fieldInitialisers.FieldInitialiser;
 import java.util.Comparator;
 
 public class FieldInitialiserSort implements Comparator<Class<? extends FieldInitialiser>> {
-    public FieldInitialiserSort(){
-
+    public FieldInitialiserSort() {
     }
+
     public int compare(Class<? extends FieldInitialiser> aClass, Class<? extends FieldInitialiser> t1) {
         return aClass.getSimpleName().compareTo(t1.getSimpleName());
     }
