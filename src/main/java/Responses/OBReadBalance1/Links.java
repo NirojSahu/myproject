9fa17ebb28881043affa48a975aa5c1a6da0b627
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 14:52:17 +0530

    Update Links.java

--- src/main/java/Responses/OBReadBalance1/Links.java
+++ src/main/java/Responses/OBReadBalance1/Links.java
@@ -1,36 +1,20 @@
-
 package Responses.OBReadBalance1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.annotation.Generated;
-
-
-
-public class Links {
-
+public class Links
+{
     @JsonProperty("Self")
-    private String mSelf;
+    private String self;
 
-    public String getSelf() {
-        return mSelf;
+    public String getSelf ()
+    {
+        return self;
     }
 
-    public static class Builder {
-
-        private String mSelf;
-
-        public Links.Builder withSelf(String self) {
-            mSelf = self;
-            return this;
-        }
-
-        public Links build() {
-            Links links = new Links();
-            links.mSelf = mSelf;
-            return links;
-        }
-
-    }
-
+   /* @Override
+    public String toString()
+    {
+        return "ClassPojo [Self = "+Self+"]";
+    }*/
 }
