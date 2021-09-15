377887802d17d81295c0561e06eb89b86acf3073
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:03:33 +0530

    Update ScreenshotHooks.java

--- src/main/java/Utilities/WebKit/screenshothook/ScreenshotHooks.java
+++ src/main/java/Utilities/WebKit/screenshothook/ScreenshotHooks.java
@@ -1,4 +1,33 @@
-package Utilities.WebKit.screenshothook;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.screenshothook;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import org.openqa.selenium.NoSuchSessionException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 public class ScreenshotHooks {
+    @Inject
+    Provider<WebDriver> driver;
+
+    public ScreenshotHooks() {
+    }
+
+    @After
+    public void takeScreenshot(Scenario scenario) throws Exception {
+        try {
+            scenario.embed((byte[])((TakesScreenshot)this.driver.get()).getScreenshotAs(OutputType.BYTES), "image/png");
+        } catch (NoSuchSessionException var3) {
+            System.out.println("No screenshot taken for Test => " + scenario.getName());
+        }
+
+    }
 }
