a0863541e5ca52cb8690d90d59c7fc0de75b8f1d
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:57:45 +0530

    Update TimeoutHelper.java

--- src/main/java/Utilities/WebKit/helper/TimeoutHelper.java
+++ src/main/java/Utilities/WebKit/helper/TimeoutHelper.java
@@ -1,19 +1,29 @@
-package Utilities.WebKit.helper;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import java.util.concurrent.*;
+package com.test.helper;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class TimeoutHelper {
-    private TimeoutHelper(){
+    private TimeoutHelper() {
     }
-    public static void withTimeout(Integer timeout,Runnable runnable) {
-        ExecutorService sceduledExecutorService = Executors.newSingleThreadExecutor();
+
+    public static void withTimeout(Integer timeout, Runnable runnable) throws TimeoutException {
+        ExecutorService scheduledExecutorService = Executors.newSingleThreadExecutor();
+
         try {
-            sceduledExecutorService.submit(runnable).get((long) timeout, TimeUnit.MICROSECONDS);
+            scheduledExecutorService.submit(runnable).get((long)timeout, TimeUnit.MILLISECONDS);
         } catch (InterruptedException var4) {
             var4.printStackTrace();
         } catch (ExecutionException var5) {
-        } catch (TimeoutException e) {
-            e.printStackTrace();
         }
+
     }
 }
