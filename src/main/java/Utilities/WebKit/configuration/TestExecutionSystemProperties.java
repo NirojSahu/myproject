7f691287ee2a541f304d8f70da873288f7a97fbc
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:25:37 +0530

    Create TestExecutionSystemProperties.java

---
+++ src/main/java/Utilities/WebKit/configuration/TestExecutionSystemProperties.java
@@ -0,0 +1,27 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestExecutionSystemProperties {
+    private final Properties properties = new Properties();
+
+    public TestExecutionSystemProperties() {
+        try {
+            this.properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("com.test.defaults.properties"));
+        } catch (IOException var2) {
+            var2.printStackTrace();
+        }
+
+        this.properties.putAll(System.getProperties());
+    }
+
+    public Properties getProperties() {
+        return this.properties;
+    }
+}
