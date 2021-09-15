1839f1de203ec0835fa22c83ee7e225d7423305a
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 15:55:02 +0530

    Update PageIdHelper.java

--- src/main/java/Utilities/WebKit/helper/PageIdHelper.java
+++ src/main/java/Utilities/WebKit/helper/PageIdHelper.java
@@ -1,4 +1,9 @@
-package Utilities.WebKit.helper;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.helper;
 
 public interface PageIdHelper {
     String getPageId();
