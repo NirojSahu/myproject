ae0b2e5c8cb724d64054cd892cc4d9ee07006faf
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-15 16:03:10 +0530

    Create PropertyReader.java

---
+++ src/main/java/Utilities/WebKit/lynx/PropertyReader.java
@@ -0,0 +1,32 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.lynx;
+
+public class PropertyReader {
+    private static final String LYNX_URL = "http://lynxpreoff.unix.santanderuk.pre.corp/comunes/VAuth.php";
+    private static final String LYNX_USERNAME = "MinUsu";
+    private static final String LYNX_PASSWORD = "san336699";
+    private static final String LYNX_SYSTYPE = "Card/Account";
+
+    public PropertyReader() {
+    }
+
+    public static String getLynxUrl() {
+        return "http://lynxpreoff.unix.santanderuk.pre.corp/comunes/VAuth.php";
+    }
+
+    public static String getLynxUsername() {
+        return "MinUsu";
+    }
+
+    public static String getLynxPassword() {
+        return "san336699";
+    }
+
+    public static String getLynxSystype() {
+        return "Card/Account";
+    }
+}
