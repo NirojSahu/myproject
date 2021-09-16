5df2d9a936dc45f5d2bb8f029af93eb5178febd1
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-16 13:09:09 +0530

    Update PageObjectModelInjectionListener.java

--- src/main/java/Utilities/seleniumcustomframeworkguice/PageObjectModelInjectionListener.java
+++ src/main/java/Utilities/seleniumcustomframeworkguice/PageObjectModelInjectionListener.java
@@ -1,4 +1,24 @@
-package Utilities.seleniumcustomframeworkguice;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-public class PageObjectModelInjectionListener {
+package com.test.seleniumcustomframework.guice;
+
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+import com.google.inject.spi.InjectionListener;
+import com.test.seleniumcustomframework.extension.PageFactory;
+
+class PageObjectModelInjectionListener implements InjectionListener {
+    private Provider<Injector> injector;
+
+    public PageObjectModelInjectionListener(Provider<Injector> injector) {
+        this.injector = injector;
+    }
+
+    public void afterInjection(Object page) {
+        PageFactory pageFactory = (PageFactory)((Injector)this.injector.get()).getInstance(PageFactory.class);
+        pageFactory.get(page);
+    }
 }
