3eaf6cf086f018f436aeafd6d4af5bbcca9397d6
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-16 13:09:58 +0530

    Create SeleniumPomGuiceModule.java

---
+++ src/main/java/Utilities/seleniumcustomframeworkguice/SeleniumPomGuiceModule.java
@@ -0,0 +1,65 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.seleniumcustomframework.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.Stage;
+import com.google.inject.matcher.Matchers;
+import com.test.seleniumcustomframework.extension.PageFactory;
+import com.test.seleniumcustomframework.extension.dependencies.DependencyFactory;
+import com.test.seleniumcustomframework.extension.dependencies.DependencyInjector;
+import com.test.seleniumcustomframework.extension.dependencies.InjectionError;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class SeleniumPomGuiceModule extends AbstractModule implements DependencyInjector {
+    private Injector injector;
+    private HashMap<Class, DependencyFactory> factories = new HashMap();
+    private PageFactory pageFactory;
+    private ArrayList<Module> modules = new ArrayList();
+
+    public SeleniumPomGuiceModule(Module... modules) {
+        this.modules.add(this);
+        Collections.addAll(this.modules, modules);
+    }
+
+    protected void configure() {
+        this.bindListener(Matchers.any(), new PageObjectModelTypeListener());
+    }
+
+    @Provides
+    @Singleton
+    PageFactory providePageFactory() {
+        if (this.pageFactory != null) {
+            return this.pageFactory;
+        } else {
+            this.pageFactory = new PageFactory(this, new DependencyFactory[0]);
+            return this.pageFactory;
+        }
+    }
+
+    public Injector getInjector() {
+        if (this.injector == null) {
+            this.injector = Guice.createInjector(Stage.PRODUCTION, this.modules);
+        }
+
+        return this.injector;
+    }
+
+    public <T> T get(Class<T> aClass) throws InjectionError {
+        return this.getInjector().getInstance(aClass);
+    }
+
+    public void injectMembers(Object object) {
+        this.getInjector().injectMembers(object);
+    }
+}
