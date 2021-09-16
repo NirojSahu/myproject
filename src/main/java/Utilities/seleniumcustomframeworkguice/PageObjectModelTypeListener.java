3f4b9ac4fe06af0820956488c0f084565fb57fcb
NirojSahu <69239574+NirojSahu@users.noreply.github.com>
2021-09-16 13:09:37 +0530

    Create PageObjectModelTypeListener.java

---
+++ src/main/java/Utilities/seleniumcustomframeworkguice/PageObjectModelTypeListener.java
@@ -0,0 +1,40 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.test.seleniumcustomframework.guice;
+
+import com.google.inject.Injector;
+import com.google.inject.TypeLiteral;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
+import com.test.seleniumcustomframework.extension.fieldInitialisers.FieldAssessor;
+import com.test.seleniumcustomframework.extension.helpers.ReflectionHelper;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+
+class PageObjectModelTypeListener implements TypeListener {
+    PageObjectModelTypeListener() {
+    }
+
+    public <I> void hear(TypeLiteral<I> typeLiteral, TypeEncounter<I> typeEncounter) {
+        if (this.isPageObject(typeLiteral.getRawType())) {
+            typeEncounter.register(new PageObjectModelInjectionListener(typeEncounter.getProvider(Injector.class)));
+        }
+
+    }
+
+    private boolean isPageObject(Class<?> rawType) {
+        Iterator var2 = ReflectionHelper.getAllClassFields(rawType).iterator();
+
+        while(var2.hasNext()) {
+            Field field = (Field)var2.next();
+            if (FieldAssessor.isSeleniumPomField(field)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
